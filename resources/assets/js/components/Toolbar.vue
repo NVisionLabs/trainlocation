fe0528207ded60b06c8efbc62fadcbcf202d17f5
NVisionLabs <aliwer406@gmail.com>
2018-06-01 09:50:47 +0100

    Login component and installed vue.

---
+++ resources/assets/js/components/Toolbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-toolbar>
+  <!--  <v-toolbar-side-icon></v-toolbar-side-icon> -->
+    <v-toolbar-title>Train Location</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <div class="hidden-sm-and-down">
+      <router-link to="/">
+        <v-btn flat>Location</v-btn>
+      </router-link>
+      <router-link to="/login"> 
+      <v-btn flat>Login</v-btn>
+      </router-link>
+    </div>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
